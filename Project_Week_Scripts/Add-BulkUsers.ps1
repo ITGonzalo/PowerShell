e372f19042778202cc0b7d36d27d0c9c7fd3cdee
ITGonzalo <gonzalo.colarte@outlook.com>
2023-09-13 01:44:20 +0900

    still debugging, can't log in with user creds

---
+++ Project_Week_Scripts/Add-BulkUsers.ps1
@@ -0,0 +1,33 @@
+#function Add-BulkUsers {
+    <# import info from csv
+       assumes the follow:
+       headers: firstname,middleinitial,lastname,username,email,streetaddress,city,zipcode,state,count
+       samaccount name is first name?
+       figuring out what needs to be set to what was confusing, did my best
+    #>
+
+    # creates the account but can't log in
+    $FilePath = "C:\Users\Administrator.ADATUM\Desktop\users.csv"
+    $UsersFromFile = Import-Csv -Path $FilePath
+    foreach ($_ in $UsersFromFile) {
+        $splat = @{
+        displayname = "$($_.firstname) $($_.middleinitial) $($_.lastname)"
+        accountpassword = (ConvertTo-SecureString $_.password -AsPlainText -Force)
+        givenname = $_.firstname
+        surname = $_.lastname
+        UserPrincipalName = "$($_.username)@$($_.maildomain)"
+        emailaddress = $_.email
+        streetaddress = $_.streetaddress
+        city = $_.city
+        postalcode = $_.zipcode
+        state = $_.state
+        country = $_.countrycode
+        department = $_.department
+        officephone = $_.telephone
+        title = $_.jobtitle
+        company = $_.company
+        enabled = $true
+        }
+        New-ADUser -Name $_.firstname -Path $_.ou @splat
+    }
+#}
