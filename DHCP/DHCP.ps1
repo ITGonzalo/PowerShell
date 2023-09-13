aa1a2a6619b15820273e32ec61e559e38e421ad9
ITGonzalo <gonzalo.colarte@outlook.com>
2023-09-14 07:48:58 +0900

    Trying to minimize lines of code

--- DHCP/DHCP.ps1
+++ DHCP/DHCP.ps1
@@ -1,26 +1,24 @@
 # Still a work in progress
 # need a live environment to test things out or build a test one from the ground up
-
-# Maybe replace subnet masks with CIDR notation
+# Maybe replace subnet masks with CIDR notation using calculated properties for fun
 
 $DHCPServer = "DHCP Server"
 
 $AllDHCPScopes = Get-DHCPServerV4Scope -ComputerName $DHCPServer 
 
-$Scopes = $AllDHCPScopes | Select-Object ScopeId, Name, State, SubnetMask
+$Scopes = $AllDHCPScopes | 
+    Select-Object ScopeId, Name, State, SubnetMask | 
+    Format-Table
 
 $Leases = $AllDHCPScopes | ForEach-Object {
     Get-DHCPServerv4Lease -ComputerName $DHCPServer -ScopeID $_.ScopeID | 
-    Select-Object ScopeId, IPAddress, HostName, ClientID, AddressState
-    } | Out-GridView
-
-$Reservations = $AllDHCPScopes | 
-    ForEach-Object {
-        Get-DHCPServerv4Lease -ScopeID $_.ScopeID | 
-        Where-Object {$_.AddressState -like '*Reservation'} | 
-        Select-Object ScopeId, IPAddress, HostName, @{n="MACAddress"; e={$_.ClientID}}, AddressState, LeaseExpiryTime |
-        Format-Table
+    Select-Object ScopeId, IPAddress, HostName, ClientID, AddressState | 
+    Format-Table
     }
+
+$Reservations = $Leases | 
+    Where-Object {$_.AddressState -like '*Reservation'} | 
+    Format-Table
 
 # Try to create a non-gross way to display these nicely eventually
 $Scopes
