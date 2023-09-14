d6e512d07ba2814327a490da60c5acf250529d12
ITGonzalo <gonzalo.colarte@outlook.com>
2023-09-15 05:36:27 +0900

    minor changes

--- Old stuff that needs love/Get-ComputerUptime.ps1
+++ Old stuff that needs love/Get-ComputerUptime.ps1
@@ -5,7 +5,16 @@ $Computers = "LON-SVR1", "LON-SVR2", "LON-SVR3"
 
 Foreach ($_ in $Computers) {
     Invoke-Command -ComputerName $_ -ScriptBlock {
-        $UsableTimeFormat = Get-CimInstance -ComputerName $_ win32_operatingsytem | Select-Object -ExpandProperty LastBootUpTimee
-        Write-Output "$env:COMPUTERNAME $UsableTimeFormat"
+        # Get Last Boot Up Time
+        $UsableTimeFormat = Get-CimInstance -ComputerName $_ win32_operatingsytem | Select-Object -ExpandProperty LastBootUpTime
+        
+        # Create timespan object by subtracting the old date from the current date
+        $TimeSinceLastBootUp = ((Get-Date) - $UsableTimeFormat)
+        
+        # Start from the old and end with the new to avoid negative values
+        # New-TimeSpan -Start $UsableTimeFormat -End $(Get-Date)
+
+        # The time it's been between dates
+        $TimeSinceLastBootUp
     }
 }
--- WMI and CIM Queries/Get-ComputerSerial.ps1
+++ WMI and CIM Queries/Get-ComputerSerial.ps1
@@ -1,3 +1,6 @@
 $SerialNumber = Get-WmiObject -ComputerName $ComputerName -Class Win32_BIOS | Select-Object -ExpandProperty SerialNumber
 
+# CIM approach:
+# Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber
+
 $SerialNumber
